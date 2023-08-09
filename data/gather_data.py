057c718ae2db898bef9de1ead17a9d55c50ca1d6
Emiko Bell <emikoelizabethbell@gmail.com>
2023-08-08 17:35:36 -0700

    Create Firebase collection and add job information

--- data/gather_data.py
+++ data/gather_data.py
@@ -1,12 +1,12 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 from secrets import li_username, li_pass
 from utils import process_job_item, extract_job_list, find_next_job
+from database import create_job
 import time
 
 def extract_li_data():
-    # for testing
-    all_jobs = []
     driver = webdriver.Chrome()
     driver.get("https://www.linkedin.com/login")
 
@@ -34,21 +34,21 @@ def extract_li_data():
     # Wait until the page renders completely
     time.sleep(5)
 
-    # The next section should loop over each job item in the page. The job items are a li with data-occludable-job-id or a normal html id with "ember" + three digit integer.
-    # Possible strategy:
-        # for each li in specific div (check id), execute. Once all are done, change the page and redo the whole process
-        # I would need to identify the "endpoint" for all search results.
+    job_list = driver.find_elements(By.CSS_SELECTOR, ".scaffold-layout__list-container > li")
     
-    # I need to scroll through the job list at page render to load all of the jobs.
-    
-    job_list = extract_job_list(driver)
-
     for li in job_list:
+        driver.execute_script("arguments[0].scrollIntoView()", li)
+        time.sleep(2)
+
+        try:
+            li.find_element(By.CSS_SELECTOR, "div")
+        except NoSuchElementException:
+            time.sleep(3)
+
         job_item = process_job_item(driver)
-        all_jobs.append(job_item)
+        create_job(job_item)
         next_job_id = find_next_job(driver, job_item["job_id"])
         driver.find_element(By.XPATH, f"//div[@data-job-id='{next_job_id}']").click()
-        time.sleep(2)
 
     driver.quit()
 
--- data/utils.py
+++ data/utils.py
@@ -9,18 +9,19 @@ def process_job_item(driver):
     parsed_url = urlparse(url)
     # Get job ID from the URL query parameter
 
+    print("The html is:", html)
+
     try:
         job_item["job_id"] = parse_qs(parsed_url.query).get("currentJobId", None)[0]
         # Company name -> child of div class "jobs-unified-top-card__primary-description" > div > a
-        job_item["company_name"] = html.css.select(".jobs-unified-top-card__primary-description a")[0].get_text().strip()
+        job_item["company_name"] = html.css.select(".jobs-unified-top-card__primary-description a")[0].get_text().strip()\
+            if html.css.select(".jobs-unified-top-card__primary-description a") else None
         # LinkedIn job level category -> child of li class "jobs-unified-top-card__job-insight" > span > after center dot e.g. ("Full-time · Mid-Senior level")
         job_item["li_level"] = html.css.select(".jobs-unified-top-card__job-insight > span")[0].get_text().split("· ")[1].strip()
         # Job title -> h2 class "jobs-unified-top-card__job-title"
         job_item["job_title"] = html.css.select(".jobs-unified-top-card__job-title")[0].get_text().strip()
-        # Job description -> div id = "job-details"
-        job_item["job_details_html"] = html.find("div", id = "job-details")
         # String only version 
-        job_item["job_details_string"] = job_item["job_details_html"].get_text(" ")
+        job_item["job_details"] = html.find("div", id = "job-details").get_text(" ")
 
     except:
         raise ValueError("There was an error when finding job information."
