e369e8000f291bf6545160d2ad7db31d85fc5542
MonAmez <100624293+MonAmez@users.noreply.github.com>
2023-09-04 21:52:44 -0700

    Add function to expand contractions

--- preprocess_data/text_cleaning.py
+++ preprocess_data/text_cleaning.py
@@ -66,13 +66,80 @@ def remove_email_address(text):
 def remove_urls(text):
     url_pattern = re.compile(r'(?:http[s]?://)(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+|'
                              r'check us out on|visit our website|learn more at | follow us at | follow us on | add us on |'
-                             r'email us at | email at',
+                             r'email us at | email at | visit',
                              flags=re.IGNORECASE)
 
     cleaned_text = re.sub(url_pattern, '', text)
 
     return cleaned_text
 
+def remove_punctuation(text):
+    # Use regular expression to match and replace punctuation with spaces
+    cleaned_text = re.sub(r'(?<=[^\w\s\.\-])|(?=[^\w\s\.\-])', ' ', text)
+    return cleaned_text
+
+
+def expand_contractions(text):
+    contractions_dict = {
+        "ain't": "are not",
+        "aren't": "are not",
+        "can't": "cannot",
+        "couldn't": "could not",
+        "didn't": "did not",
+        "doesn't": "does not",
+        "don't": "do not",
+        "hadn't": "had not",
+        "hasn't": "has not",
+        "haven't": "have not",
+        "he's": "he is",
+        "how'd": "how did",
+        "how'll": "how will",
+        "how's": "how is",
+        "I'd": "I would",
+        "I'll": "I will",
+        "I'm": "I am",
+        "I've": "I have",
+        "isn't": "is not",
+        "it's": "it is",
+        "let's": "let us",
+        "mustn't": "must not",
+        "shan't": "shall not",
+        "she'd": "she would",
+        "she'll": "she will",
+        "she's": "she is",
+        "shouldn't": "should not",
+        "that's": "that is",
+        "there's": "there is",
+        "they'd": "they would",
+        "they'll": "they will",
+        "they're": "they are",
+        "they've": "they have",
+        "we'd": "we would",
+        "we'll": "we will",
+        "we're": "we are",
+        "we've": "we have",
+        "weren't": "were not",
+        "what'll": "what will",
+        "what're": "what are",
+        "what's": "what is",
+        "what've": "what have",
+        "where's": "where is",
+        "who'd": "who would",
+        "who'll": "who will",
+        "who're": "who are",
+        "who's": "who is",
+        "who've": "who have",
+        "won't": "will not",
+        "wouldn't": "would not",
+        "you'd": "you would",
+        "you'll": "you will",
+        "you're": "you are",
+        "you've": "you have",
+    }
+
+    for contraction, expansion in contractions_dict.items():
+        text = text.replace(contraction, expansion)
+    return text
 
 def clean_job_details(df):
     # Remove any text markup from LinkedIn
@@ -99,9 +166,16 @@ def clean_job_details(df):
     print('Removing email addresses...')
     df['cleaned_job_details'] = df['cleaned_job_details'].apply(remove_email_address)
 
-    # Removing email addresses...
+    # Removing URLs...
     print('Removing URLs...')
     df['cleaned_job_details'] = df['cleaned_job_details'].apply(remove_urls)
+
+    # Removing punctuations
+    print('Removing punctuations...')
+    df['cleaned_job_details'] = df['cleaned_job_details'].apply(remove_punctuation)
+
+    print("Expanding contractions...")
+    df['cleaned_job_details'] = df['cleaned_job_details'].apply(expand_contractions)
 
     print('Correcting typos...')
     df['cleaned_job_details'] = autocorrect_typos(df['cleaned_job_details'])
