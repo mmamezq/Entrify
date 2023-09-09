da6342ebb9f642e072ea125d0846d1c2b8a30be8
MonAmez <100624293+MonAmez@users.noreply.github.com>
2023-09-08 21:05:10 -0700

    Tokenize, compute 95th percentile sequence length, and pad/truncate to this length

--- preprocess_data/bert_pipeline/vectorizer.py
+++ preprocess_data/bert_pipeline/vectorizer.py
@@ -1,16 +1,28 @@
-import torch
 from transformers import BertTokenizer, BertModel
+from tensorflow.keras.utils import pad_sequences
 
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertModel.from_pretrained("bert-base-uncased")
 
-
 def tokenize_text(text):
-    doc = nlp(text)
-    tokens = [token.text for token in doc]
-    return tokens
+    tokenized_output = tokenizer(text)
+    tokens = tokenized_output['input_ids']
+    return tokenized_output, tokens
 
-def tokenize(df):
+def compute_length(df):
+    df['sequence_length'] = df['token_ids'].apply(len)
+    percentile_95_length = df['sequence_length'].quantile(0.95)
+    return percentile_95_length
+
+def pad_text_sequences(seq, max_length):
+    padded_sequence = pad_sequences(seq, maxlen = max_length, padding = 'post', truncating = 'post')
+
+
+def vectorize(df):
     print('Tokenizing cleaned text...')
-    df['tokens'] = df['cleaned_job_details'].apply(tokenize_text)
+    tokenized_ouput, tokens = df['cleaned_job_details'].apply(tokenize_text)
+    df['bert_tokenizer_data'] = tokenized_ouput
+    df['token_ids'] = tokens
+    max_length = compute_length(df['token_ids'])
+
     return df
